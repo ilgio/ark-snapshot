96d1b27cd9727e96724a6d9c1f7a4a804fe20343
ilgio <isolagio@gmail.com>
2017-03-09 19:53:49 +0100

    ark-snapshot.sh

---
+++ ark-snapshot.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+VERSION="0.1"
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+
+#============================================================
+#= snapshot.sh v0.1 created by ilgio                        =
+#= Please consider voting for delegate ilgio                =
+#============================================================
+echo " "
+
+if [ ! -f ../ark-node/app.js ]; then
+  echo "Error: No ark installation detected. Exiting."
+  exit 1
+fi
+
+if [ "\$USER" == "root" ]; then
+  echo "Error: ARK should not be run be as root. Exiting."
+  exit 1
+fi
+
+DB_NAME="ark_testnet"
+DB_USER="ark"
+DB_PASS="password"
+SNAPSHOT_COUNTER=snapshot/counter.json
+SNAPSHOT_LOG=snapshot/snapshot.log
+if [ ! -f "snapshot/counter.json" ]; then
+  mkdir -p snapshot
+  sudo chmod a+x ark-snapshot.sh
+  echo "0" > $SNAPSHOT_COUNTER
+  sudo chown postgres:${USER:=$(/usr/bin/id -run)} snapshot
+  sudo chmod -R 777 snapshot
+fi
+SNAPSHOT_DIRECTORY=snapshot/
+
+
+NOW=$(date +"%d-%m-%Y - %T")
+################################################################################
+
+create_snapshot() {
+  export PGPASSWORD=$DB_PASS
+  echo " + Creating snapshot"
+  echo "--------------------------------------------------"
+  echo "..."
+  sudo su postgres -c "pg_dump -Ft $DB_NAME > $SNAPSHOT_DIRECTORY'ark_db$NOW.snapshot.tar'"
+  blockHeight=`psql -d $DB_NAME -U $DB_USER -h localhost -p 5432 -t -c "select height from blocks order by height desc limit 1;"`
+  dbSize=`psql -d $DB_NAME -U $DB_USER -h localhost -p 5432 -t -c "select pg_size_pretty(pg_database_size('$DB_NAME'));"`
+
+  if [ $? != 0 ]; then
+    echo "X Failed to create snapshot." | tee -a $SNAPSHOT_LOG
+    exit 1
+  else
+    echo "$NOW -- OK snapshot created successfully at block$blockHeight ($dbSize)." | tee -a $SNAPSHOT_LOG
+  fi
+
+}
+
+restore_snapshot(){
+  echo " + Restoring snapshot"
+  echo "--------------------------------------------------"
+  SNAPSHOT_FILE=`ls -t snapshot/ark_db* | head  -1`
+  if [ -z "$SNAPSHOT_FILE" ]; then
+    echo "****** No snapshot to restore, please consider create it first"
+    echo " "
+    exit 1
+  fi
+  echo "Snapshot to restore = $SNAPSHOT_FILE"
+
+  read -p "Please stop node app.js first, are you ready (y/n)? " -n 1 -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+     echo "***** Please stop node.js first.. then execute restore again"
+     echo " "
+     exit 1
+  fi
+
+#snapshot restoring..
+  export PGPASSWORD=$DB_PASS
+  pg_restore -d $DB_NAME "$SNAPSHOT_FILE" -U $DB_USER -h localhost -c -n public
+
+  if [ $? != 0 ]; then
+    echo "X Failed to restore."
+    exit 1
+  else
+    echo "OK snapshot restored successfully."
+  fi
+
+}
+
+show_log(){
+  echo " + Snapshot Log"
+  echo "--------------------------------------------------"
+  cat snapshot/snapshot.log
+  echo "--------------------------------------------------END"
+}
+
+################################################################################
+
+case $1 in
+"create")
+  create_snapshot
+  ;;
+"restore")
+  restore_snapshot
+  ;;
+"log")
+  show_log
+  ;;
+"hello")
+  echo "Hello my friend - $NOW"
+  ;;
+"help")
+  echo "Available commands are: "
+  echo "  create   - Create new snapshot"
+  echo "  restore  - Restore the last snapshot available in folder snapshot/"
+  echo "  log      - Display log"
+  ;;
+*)
+  echo "Error: Unrecognized command."
+  echo ""
+  echo "Available commands are: create, restore, log, help"
+  echo "Try: bash ark-snapshot.sh help"
+  ;;
+esac
+                                                                                                                                                    
